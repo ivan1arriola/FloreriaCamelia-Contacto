f7d8b2d5d0a7b285fb0349ebde477e42e9c21893
ivan1arriola <ivan.arriola@fing.edu.uy>
2023-02-09 00:11:52 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
 const indexRoutes = require("./routes/index.routes");
+const cors = require("cors");
+
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(indexRoutes);
